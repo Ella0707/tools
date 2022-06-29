979028bb0891d296d7bcbbe45dafcd02fd502e16
unknown <pakelvi0707@gmail.com>
2022-06-29 15:25:58 +0300

    work

--- js/main.js
+++ js/main.js
@@ -1,115 +1,42 @@
-function show_hide_password(target) {
-    let input = document.getElementById('password');
-    if (input.getAttribute('type') == 'password') {
-        target.classList.add('view');
-        input.setAttribute('type', 'text');
-    } else {
-        target.classList.remove('view');
-        input.setAttribute('type', 'password');
-    }
-    return false;
-}
+"use strict";
 
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
 
-window.addEventListener("DOMContentLoaded", function () {
-    [].forEach.call(document.querySelectorAll('.tel'), function (input) {
-        var keyCode;
-        function mask(event) {
-            event.keyCode && (keyCode = event.keyCode);
-            var pos = this.selectionStart;
-            if (pos < 3) event.preventDefault();
-            var matrix = "+7 (___) ___ ____",
-                i = 0,
-                def = matrix.replace(/\D/g, ""),
-                val = this.value.replace(/\D/g, ""),
-                new_value = matrix.replace(/[_\d]/g, function (a) {
-                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-                });
-            i = new_value.indexOf("_");
-            if (i != -1) {
-                i < 5 && (i = 3);
-                new_value = new_value.slice(0, i)
-            }
-            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-                function (a) {
-                    return "\\d{1," + a.length + "}"
-                }).replace(/[+()]/g, "\\$&");
-            reg = new RegExp("^" + reg + "$");
-            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-            if (event.type == "blur" && this.value.length < 5) this.value = ""
-        }
-
-        input.addEventListener("input", mask, false);
-        input.addEventListener("focus", mask, false);
-        input.addEventListener("blur", mask, false);
-        input.addEventListener("keydown", mask, false)
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
     });
-});
 
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
 
-
-window.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
-    let out = document.querySelector(".out");
-    let outLink = document.querySelector(".link");
-
-    async function formSend(e) {
-        e.preventDefault();
-
-        let error = formValidate(form);
-
-        if (error === 0) {
-            // код для отправки формы
-
-        } else {
-            alert("Заполните, пожалуйста, обязательные поля");
-        }
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
     }
 
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll(".req");
-
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
-
-            if (input.classList.contains(".email"));
-            if (emailTest(input)) {
-                formAddError(input);
-                error++;
-            } 
-
-            if (input.classList.contains(".input-inn"));
-            if (input.value === "123") {
-                out.innerHTML = "Ваша компания уже зарегистрирована,пожалуйста обратитесь&nbsp;к менеджеру вашего аккаунта или";
-                outLink.innerHTML = "напишите нам";
-                formAddError(input);
-                error++;
-            } 
-            
-            else {
-                if (input.value === "") {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
-        return error;
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.select__current');
+        currentText.innerText = text;
+        select.classList.remove('is-active');
     }
+};
+select();
 
-    function formAddError(input) {
-        input.parentElement.classList.add('error');
-        input.classList.add('error');
-    }
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('error');
-        input.classList.remove('error');
-    }
 
-    function emailTest(input) {
-        return /.+@globex\.com/.test(input.value);
-    }
-});
+
+
+
+let searchBtn = document.querySelector(".header__btn-search");
+let searchInput = document.querySelector(".header__input-search");
+let searchWrap = document.querySelector(".header__search");
+
+searchBtn.onclick  = () => {
+    searchBtn.classList.toggle('active');
+    searchInput.classList.toggle('active');
+    searchWrap.classList.toggle('active');
+}
 
